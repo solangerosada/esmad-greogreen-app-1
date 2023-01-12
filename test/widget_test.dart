44f2b6e58b53ed289bf35c8a0ef260eb4dc3d49e
solangerosada <sarosadadomingos@gmail.com>
2023-01-12 07:39:21 +0000

    changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:geogreen/main.dart';
+
+// ignore: depend_on_referenced_packages
+//import 'package:geoapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
